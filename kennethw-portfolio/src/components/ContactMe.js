4cdcbe959d4d97a3205c5a5d488fd3961ab8981f
Kenneth Williams <kwilliam08@gmail.com>
2019-12-01 19:16:41 -0500

    additional tweaks

--- kennethw-portfolio/src/components/ContactMe.js
+++ kennethw-portfolio/src/components/ContactMe.js
@@ -6,7 +6,7 @@ function ContactMe() {
 
         <div className="contact" id="contact-me">
             <h1>CONTACT</h1>
-            <p>Feel free to contact me at <a href="kwilliam08@gmail.com">kwilliam08@gmail.com</a>.</p>
+            <p>Feel free to contact me at <a className="email" href="kwilliam08@gmail.com">kwilliam08@gmail.com</a>.</p>
         </div>
            
     ); 
