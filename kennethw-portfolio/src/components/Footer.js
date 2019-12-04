a6ec040847f9efdb972e7690e3bf2232e528ba71
Kenneth Williams <kwilliam08@gmail.com>
2019-12-04 11:20:49 -0500

    updated css for the footer

--- kennethw-portfolio/src/components/Footer.js
+++ kennethw-portfolio/src/components/Footer.js
@@ -6,8 +6,8 @@ function Footer() {
 
         <div className="footer">
             <p>And follow me at</p>
-            <a href="https://www.linkedin.com/in/kenneth-williams-0606419/"><img className="linkedin" alt="Linkedin" src={require('../images/LI-In-Bug.png')}/></a>
-            <a href="https://github.com/KennethMWJr"><img className="github" alt="github" src={require('../images/GitHub-Mark-Light-120px-plus.png')}/></a>
+            <a href="https://www.linkedin.com/in/kenneth-williams-0606419/"><img className="linkedin" alt="Linkedin" src={require('../images/linkedin.png')} srcSet={`${require('../images/linkedin@2x.png')} 2x`}/></a>
+            <a href="https://github.com/KennethMWJr"><img className="github" alt="github" src={require('../images/github.png')} srcSet={`${require('../images/github@2x.png')} 2x`}/></a>
         </div>
            
     ); 
